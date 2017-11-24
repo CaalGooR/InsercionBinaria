918fc5385b232802838482ec524019467fd6c4e1
Charlie <carlos.alejandro.gonzalez.ortiz@uabc.edu.mx>
2017-11-23 17:04:36 -0800

    Metodo que ajusta la posicion de los componentes graficos

--- Ventana.java
+++ Ventana.java
@@ -6,6 +6,7 @@
 */
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
 import java.lang.Thread;
 import java.util.ArrayList;
 import java.awt.Color;
@@ -13,6 +14,7 @@ import java.awt.Color;
 public class Ventana extends JFrame {
 
   private int incremento;
+  private JButton btnAjustar;
   private Objeto primero;
   private Objeto centro;
   private Objeto ultimo;
@@ -40,6 +42,16 @@ public class Ventana extends JFrame {
     new Ordenamiento(primero,centro,ultimo,auxiliar,obj,array);
   }
 
+  private void ajustarObjetos() {
+    int x = 5;
+    for(Objeto object: obj) {
+      object.setLocation(x,600);
+      object.setOpaque(true);
+      this.add(object);
+      x = incremento + object.getLocation().x;
+    }
+  }
+
   private void inicializarObjetos() {
     // Componente Limite superior Izquierdo
     primero.setBounds(10,10,100,20);
@@ -60,11 +72,16 @@ public class Ventana extends JFrame {
     auxiliar.setOpaque(true);
     auxiliar.setBackground(Color.ORANGE);
 
-    //  Variable "incremento" ayuda a ajustar a una distancia adecuada los componentes
-    //  de la clase Objeto.java
+    btnAjustar = new JButton("Ajustar");
+    btnAjustar.setBounds(230,300,80,30);
+
+    /*
+    * Variable "incremento" ayuda a ajustar a una distancia adecuada los componentes
+    * de la clase Objeto.java
+    */
     incremento = ((this.getWidth())/(array.length-1))-10;
-    // Margen los objetos con respecto a los limites del JFrame
-    int x = 5;
+
+    int x = 5; // Margen los objetos con respecto a los limites del JFrame
     for (int i = 0 ;i<array.length;i++) {
       Objeto objInicial = new Objeto(array[i]);
       obj.add(objInicial);
@@ -74,6 +91,7 @@ public class Ventana extends JFrame {
     this.add(centro);
     this.add(ultimo);
     this.add(auxiliar);
+    this.add(btnAjustar);
 
     for(Objeto object: obj) {
       object.setLocation(x,600);
